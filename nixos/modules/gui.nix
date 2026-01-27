d15375bf4f4d83f52c7104cffbef14ec011509dd
evgeniyfimushkin <fimnames@gmail.com>
2026-01-27 17:00:27 +0700

    edit foliate, change theme and wallpapers

--- nixos/modules/gui.nix
+++ nixos/modules/gui.nix
@@ -4,12 +4,13 @@
     telegram-desktop
     anki-bin
     obsidian
-    webcord
     discord
     teamspeak6-client
     libreoffice-qt
     google-chrome
     transmission_4-gtk
+    gimp
+    foliate
   ];  
 
   programs = {
