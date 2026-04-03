8ffd234387059e86d3cf11bbfc3c652010c1c34c
evgeniyfimushkin <fimnames@gmail.com>
2026-04-03 16:16:12 +0700

    k3s server

--- nixos/modules/server/k3s.nix
+++ nixos/modules/server/k3s.nix
@@ -12,6 +12,7 @@
   };
 
   systemd.tmpfiles.rules = [
+    "Z /etc/rancher/k3s/k3s.yaml 640 ${config.my.username} ${config.my.username}"
     "L+ /home/${config.my.username}/.kube/k3s.yaml - ${config.my.username} ${config.my.username} - - /etc/rancher/k3s/k3s.yaml"
   ];
 }
