42f204ea06f1c6f269e9ad2c7b77d89825c634ef
evgeniyfimushkin <fimnames@gmail.com>
2026-04-03 16:25:27 +0700

    asdf

--- nixos/modules/server/k3s.nix
+++ nixos/modules/server/k3s.nix
@@ -10,8 +10,4 @@
     enable = true;
     role = "server";
   };
-
-  systemd.tmpfiles.rules = [
-    "C /home/${config.my.username}/.kube/k3s.yaml 640 ${config.my.username} ${config.my.username} - /etc/rancher/k3s/k3s.yaml"
-  ];
 }
