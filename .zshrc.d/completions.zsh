d0ab03fee5d7230a98b39dcb21aa370ec783968d
evgeniyfimushkin <fimushkin@gmail.com>
2026-07-05 22:32:02 +0300

    fixes

--- .zshrc.d/completions.zsh
+++ .zshrc.d/completions.zsh
@@ -1,4 +1,3 @@
-# ya tool completions
 if [[ -d "$HOME/.ya.completion/zsh" ]]; then
     fpath=("$HOME/.ya.completion/zsh" $fpath)
 fi
@@ -8,12 +7,10 @@ compinit -C
 
 setopt complete_aliases
 
-# Only load kubectl completion if kubectl is installed
 if command -v kubectl &>/dev/null; then
     source <(kubectl completion zsh)
 fi
 
-# Only set compdef for kubecolor if both kubecolor and kubectl exist
 if command -v kubecolor &>/dev/null && command -v kubectl &>/dev/null; then
     compdef kubecolor=kubectl
 fi
--- .zshrc.d/yandex.zsh
+++ .zshrc.d/yandex.zsh
@@ -5,3 +5,15 @@
 # --- Yandex utilities below ---
 
 alias infractl='ya tool infractl'
+alias ci='ya tool ci'
+alias cs='ya tool cs'
+alias yav='ya tool yav'
+
+smclusters () {
+    local host="$1"
+    ssh "$host" "curl 'http://localhost:19000/clusters?format=json' -s" \
+    | jq '.cluster_statuses[].name' \
+    | sort -u
+}
+
+[ -f ~/.osy-tokens.sh ] && source ~/.osy-tokens.sh
